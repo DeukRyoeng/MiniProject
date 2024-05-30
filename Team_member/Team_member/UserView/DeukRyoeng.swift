a4371f7cda850aaf896ee8e2b3c4ffef3c019da4
DeukYeong <polcet112@gmail.com>
2024-05-30 18:15:20 +0900

    득령 병합용 커밋

--- Team_member/Team_member/UserView/DeukRyoeng.swift
+++ Team_member/Team_member/UserView/DeukRyoeng.swift
@@ -10,9 +10,27 @@ import SafariServices
 
 class DeukRyoengVC: ViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let imgView: UIImageView = {
+            let aView = UIImageView()
+            imageView.layer.cornerRadius = imageView.frame.height / 2
+            imageView.layer.borderWidth = 1
+            imageView.layer.borderColor = UIColor.clear.cgColor
+            return imageView
+        }()
+        //이미지 쉐도우
+        let imgShadown: UIView = {
+            let aView = UIView()
+            aView.layer.shadowOffset = CGSize(width: 5, height: 5)
+            aView.layer.shadowOpacity = 0.7
+            aView.layer.shadowRadius = 5
+            
+            return imageView
+        }()
+        imageView.clipsToBounds = true
+       
     }
     @IBAction func backBtn(_ sender: UIButton) {
         print("called - DeukRyoengVC: click Back Btn!! ")
