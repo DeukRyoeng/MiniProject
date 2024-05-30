6393a517586044780787c77066db44ed17c1baa9
DeukYeong <polcet112@gmail.com>
2024-05-30 20:45:27 +0900

    복구

--- Team_member/Team_member/UserView/DeukRyoeng.swift
+++ Team_member/Team_member/UserView/DeukRyoeng.swift
@@ -10,9 +10,23 @@ import SafariServices
 
 class DeukRyoengVC: ViewController {
     
+    var intr: [String] = ["MBTI", "신체", "취미","거주지", "IG", "last"," "]
+    let ans: [String] = ["ENTP", "175 / 80", "복싱, 웨이트, 음주","인천 미추홀", "@dx._.xk7"]
+    var count = 0
+    
+    @IBOutlet weak var q: UILabel!
+    @IBOutlet weak var a: UILabel!
+    @IBOutlet weak var next_Btn: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        //NextBtn이미지 지정
+        let imgConfig = UIImage.SymbolConfiguration(pointSize: 60,weight: .light)
+        let img = UIImage(systemName: "arrow.right.circle.fill",withConfiguration: imgConfig)
+        next_Btn.setImage(img, for: .normal)
+        next_Btn.tintColor = .white
+        
         let imgView: UIImageView = {
             let aView = UIImageView()
             imageView.layer.cornerRadius = imageView.frame.height / 2
@@ -44,5 +58,18 @@ class DeukRyoengVC: ViewController {
         let blogSafariView: SFSafariViewController = SFSafariViewController(url: blogUrl as! URL)
         self.present(blogSafariView, animated: true, completion: nil)
         
+    }
+    @IBAction func nextBtn(_ sender: UIButton) {
+        if count >= intr.count {
+            print("Index out of range")
+        } else if intr[count] == "last" {
+            print("마지막 인덱스")
+            q.text = "마지막인뒝?"
+            a.text = "더 없눈뒝??"
+        } else {
+            q.text = intr[count]
+            a.text = ans[count]
+            count += 1
+        }
     }
 }
